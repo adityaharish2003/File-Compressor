62ba9f2ce84065080a812f1e4c51739b35bc588a
Aditya Harish <adityaharish99@gmail.com>
2023-01-02 22:26:26 +0530

    Initial Commit

---
+++ driver.py
@@ -0,0 +1,6 @@
+from HuffmanCoding import HuffmanCoding
+
+FileHandler = HuffmanCoding('/home/aditya/Desktop/dev/huffman/input.txt')
+FileHandler.compress()
+
+FileHandler.decompress('/home/aditya/Desktop/dev/huffman/input.bin')
