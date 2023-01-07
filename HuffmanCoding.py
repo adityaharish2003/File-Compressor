06ae8c149fb0013cd499397112bf308c961170d4
ronithrnair <ronithrnair1@gmail.com>
2023-01-07 15:13:07 +0530

    Driver Codes Updated + Pdf added

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -102,11 +102,11 @@ class HuffmanCoding:
 		return b
 
 
-	def compress(self):
+	def compress(self,file_ext = '.txt'):
 
 		filename, file_extension = os.path.splitext(self.path)
 		output_path = filename + ".bin"
-
+		file_extension = file_ext
 		with open(self.path, 'r+') as file, open(output_path, 'wb') as output:
 			text = file.read()
 			text = text.rstrip()
@@ -121,7 +121,7 @@ class HuffmanCoding:
 
 			b = self.get_byte_array(padded_encoded_text)
 			output.write(bytes(b))
-			delimiter =  b'\x00'
+			delimiter =  b'\x01'
 			output.write(delimiter)
 			output.write(bytes((' '.join('{0:08b}'.format(ord(x), 'b') for x in file_extension)),'utf-8'))
 			output.write(delimiter)
@@ -161,7 +161,7 @@ class HuffmanCoding:
 		output_path = filename + "_decompressed" + ".txt"
 		file_extension = ""
 		huff_code = {}
-		delimiter  = b'\x00'
+		delimiter  = b'\x01'
 		with open(input_path, 'rb') as file, open(output_path, 'w') as output:
 			bit_string = ""
 			byte = file.read(1)
@@ -173,11 +173,9 @@ class HuffmanCoding:
 				byte = file.read(1)
 	
 			encoded_text = self.remove_padding(bit_string)
-
-
 			byte = file.read(1)
 			# while(len(byte) > 0 and (byte) != bytes(' '.join('{0:08b}'.format(ord(x), 'b') for x in delimiter),encoding= 'utf-8')):
-			while(len(byte) >0 and byte != delimiter):
+			while(len(byte) >0 and byte != delimiter ):
 				file_extension += chr(ord(byte))
 				byte = file.read(1)
 			file_extension = [chr(int(i,2)) for i in file_extension.split()]
@@ -187,7 +185,6 @@ class HuffmanCoding:
 			self.reverse_mapping = rev_huff_code
 			
 			decompressed_text = self.decode_text(encoded_text)
-			
 			output.write(decompressed_text)
 
 		print("Decompressed")
--- driver.py
+++ driver.py
@@ -1,6 +1,47 @@
 from HuffmanCoding import HuffmanCoding
+import os
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+from ImageEncoding import ImageEncoding
+from PIL import Image
+import numpy as  np
+from pdf_driver import PdfEncoding
 
-FileHandler = HuffmanCoding('D:/Programs/IT206_Project/DSA_PROJECT/input.txt')
-path = FileHandler.compress()
+class driver:
+    def send():
+        Tk().withdraw()
+        path = askopenfilename()
+        print(path)
+        filename, file_extension = os.path.splitext(path)
+        shape = 0
+        enc_path = ""
+        if(file_extension == ".txt"):
+            FileHandler = HuffmanCoding(path)
+            enc_path = FileHandler.compress()
+            # out,ext = FileHandler.decompress(path)
+        elif(file_extension == ".bmp"):
+            imgenc = ImageEncoding(path)
+            shape,enc_path = imgenc.encode()
+            # imgenc.decode(enc_path,shape)
+        elif(file_extension == ".pdf"):
+            pdfenc = PdfEncoding(path)
+            enc_path = pdfenc.encode()
+        return enc_path
 
-out,ext = FileHandler.decompress(path)
+    def receive(enc_path):
+        decoding_huff = HuffmanCoding(enc_path)
+        text_path,ext = decoding_huff.decompress(enc_path)
+        # ext = '.bmp'
+        # text_path = "cat_decompressed.txt"
+        # byte_stream = np.asarray(Image.open("cat.bmp") , np.uint8)
+        # byte_string = str(byte_stream.tolist())
+        # shape = byte_stream.shape
+        # print(text_path,ext)
+        # print(shape)
+
+        if(ext == ".bmp"):
+            imgenc = ImageEncoding(text_path)
+            imgenc.txt_to_img()
+        elif(ext == ".pdf"):
+            pdfenc = PdfEncoding(text_path)
+            pdfenc.decode()
